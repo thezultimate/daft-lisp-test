c279ef8975a00c3ac96f6d4236cf38ea2a7e84d0
Dafferianto Trinugroho <DAFT@Dafferiantos-MacBook-Pro.local>
2020-09-14 22:43:25 +0200

    Missing server.lisp file

---
+++ server.lisp
@@ -0,0 +1,19 @@
+(in-package #:daft-lisp-test)
+
+(restas:define-module #:daft-lisp-test/server
+  (:use #:cl
+	#:daft-lisp-test))
+
+(in-package #:daft-lisp-test/server)
+
+(restas:define-route root ("" :method :get :content-type "text/plain")
+  (hello))
+
+(restas:define-route metrics ("metrics" :method :get :content-type "text/plain")
+  (:decorators #'restas:@no-cache)
+  "Exposing metrics endpoint"
+  (prom.text:marshal daft-lisp-test:*example-registry*))
+
+(defun hello ()
+  (prom:counter.inc daft-lisp-test:*http-requests-counter* :labels '("app"))
+  (format nil "Common Lisp is not for noobs!"))
